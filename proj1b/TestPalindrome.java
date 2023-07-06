d0aef29ac05f7750263155734212b5bc3e3cad31
wgywmm <wgywmm@gmail.com>
2023-07-06 15:34:58 +0800

    proj1b

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,17 +2,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
     @Test
     public void testWordToDeque() {
-        Deque d = palindrome.wordToDeque("persiflage");
-        String actual = "";
+        Deque<Character> d = palindrome.wordToDeque("persiflage");
+        StringBuilder actual = new StringBuilder();
         for (int i = 0; i < "persiflage".length(); i++) {
-            actual += d.removeFirst();
+            actual.append(d.removeFirst());
         }
-        assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+        assertEquals("persiflage", actual.toString());
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(palindrome.isPalindrome(""));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome("aba"));
+        assertTrue(palindrome.isPalindrome("aaccbbbccaa"));
+
+        assertFalse(palindrome.isPalindrome("ab"));
+        assertFalse(palindrome.isPalindrome("ababba"));
+    }
+
+    @Test
+    public void testIsOffByOnePalindrome() {
+        CharacterComparator cc = new OffByOne();
+        assertTrue(palindrome.isPalindrome("flake", cc));
+        assertFalse(palindrome.isPalindrome("aba", cc));
+    }
 }
